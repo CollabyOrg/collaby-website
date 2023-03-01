6f539efb81d9f79f14b3533b399baf46817fbaa6
Dacaramo <dankramirez@outlook.com>
2023-02-28 19:48:33 -0500

    Navbar created without good styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import './styles/globals.css';
+import styles from './styles/rootLayout.module.css';
 import { FC, ReactNode } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import collabyLogo from '../public/images/collaby-logo.png';
 
 interface Props {
   children: ReactNode;
@@ -8,7 +11,42 @@ interface Props {
 const RootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <div className={styles.navbarContainer}>
+          <Image
+            src={collabyLogo}
+            alt='The logo image'
+            width={100}
+            height={100}
+            priority
+          />
+          <Link
+            className={styles.link}
+            href='/'
+          >
+            Product
+          </Link>
+          <Link
+            className={styles.link}
+            href='/team'
+          >
+            Team
+          </Link>
+          <Link
+            className={styles.link}
+            href='/blog'
+          >
+            Blog
+          </Link>
+          <Link
+            className={styles.link}
+            href='/careers'
+          >
+            Careers
+          </Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 };

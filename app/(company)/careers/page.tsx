ca1165ba13110918824f8a727050ca7bcce895e5
Dacaramo <dankramirez@outlook.com>
2023-02-28 23:33:15 -0500

    Added bg image and outerContainer of the whole page

--- app/(company)/careers/page.tsx
+++ app/(company)/careers/page.tsx
@@ -1,9 +1,15 @@
+import styles from '../../styles/careersPage.module.css';
 import { FC } from 'react';
 
 interface Props {}
 
 const CareersPage: FC<Props> = ({}) => {
-  return <div>CareersPage</div>;
+  return (
+    <>
+      <div className={styles.bgImage} />
+      <div className={styles.outerContainer}>CareersPage</div>
+    </>
+  );
 };
 
 export default CareersPage;

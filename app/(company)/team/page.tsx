98ff5c114b7f90272f5bd8cdec80487fe5365894
Dacaramo <dankramirez@outlook.com>
2023-02-28 23:34:36 -0500

    Added bg image and outerContainer of the whole page

--- app/(company)/team/page.tsx
+++ app/(company)/team/page.tsx
@@ -1,9 +1,15 @@
+import styles from '../../styles/teamPage.module.css';
 import { FC } from 'react';
 
 interface Props {}
 
 const TeamPage: FC<Props> = ({}) => {
-  return <div>TeamPage</div>;
+  return (
+    <>
+      <div className={styles.bgImage} />
+      <div className={styles.outerContainer}>TeamPage</div>
+    </>
+  );
 };
 
 export default TeamPage;
